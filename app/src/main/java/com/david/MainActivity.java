72660e2149f21d1aa56a66c8b9993a606b8d655b
deeplin <10525677@qq.com>
2020-03-18 16:10:00 +0800

    update

--- app/src/main/java/com/david/MainActivity.java
+++ app/src/main/java/com/david/MainActivity.java
@@ -5,26 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import android_serialport_api.SerialPort;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+        SerialPort.loadLibrary();
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    }
 }
